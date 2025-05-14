3833b1ae5952810cb396c74bf2f87384f84fe767
施宏鑫 <811129308@qq.com>
2025-05-15 01:45:51 +0800

    复制样图到数据盘

--- dabi/util/tasks.py
+++ dabi/util/tasks.py
@@ -33,6 +33,8 @@ class DabiTaskManager:
             output_model_extension = dabi_task.config['save_model_as']
             local_model_path = os.path.join(os.getcwd(), sub_output_dir)
             output_model_size_match_epoch = False
+            timestamp = datetime.now().strftime("%Y%m%d")
+            target_file_dir = f"/mnt/train_loras/{env}/{timestamp}"
             if os.path.exists(local_model_path):
                 model_files = [model_file for model_file in os.listdir(local_model_path)
                                 if os.path.isfile(os.path.join(local_model_path, model_file))
@@ -41,7 +43,6 @@ class DabiTaskManager:
                 if max_train_epochs == len(model_files):
                     output_model_size_match_epoch = True
                     model_files.sort()
-                    timestamp = datetime.now().strftime("%Y%m%d")
                     for item in model_files:
                         # 上传模型文件时判断任务是否处于FINISHED状态，如果期间任务被中止，就不在上传
                         if self.tasks[task_id].status == TaskStatus.FINISHED:
@@ -51,7 +52,6 @@ class DabiTaskManager:
                                 "oss_path": oss_file_path
                             })
                             try:
-                                target_file_dir = f"/mnt/train_loras/{env}/{timestamp}"
                                 copy_local_file(item, local_model_path, item, target_file_dir)
                                 # 删除本地模型
                                 os.remove(os.path.join(local_model_path, item))
@@ -77,10 +77,11 @@ class DabiTaskManager:
                                 "oss_path": oss_file_path
                             })
                             try:
+                                copy_local_file(item, local_sample_image_path, item, target_file_dir)
                                 # 删除本地样图
                                 os.remove(os.path.join(local_sample_image_path, item))
                             except  Exception as e:
-                                log.error(f"Remove local sample file failed, Error fetching : {e}")
+                                log.error(f"Copy sample image to /mnt/train_loras failed, Error fetching : {e}")
                         else:
                             log.info(f"Task {task_id} may be terminated, skip upload sample image: {item}")
             if output_model_size_match_epoch:
